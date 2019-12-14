719a431d0afcbfdbf586f906eed398565b9be5e1
VLegatus <markwong167@gmail.com>
2019-12-14 15:02:03 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import  Constants from "./constants.js";
+import appReducer from './store/reducers';
+import initialState from './initialState.json';
+
+let state = initialState;
+console.log(`
+    goal: ${state.goal}
+    resorts: ${JSON.stringify(state.allSkiDays)}
+    fetching: ${state.resortNames.fetching}
+    suggestions: ${state.resortNames.suggestions}
+
+`)
+state = appReducer(state, {
+    type: Constants.SET_GOAL,
+    payload: 2
+})
+state = appReducer(state, {
+    type: Constants.ADD_DAY,
+    payload:{
+        "resort": "Mt Shasta",
+        "date": "2016-10-28",
+        "powder": false,
+        "backcountry": true
+    }
+})
+
+state = appReducer(state, {
+    type: Constants.CHANGE_SUGGESTIONS,
+    payload: ["Mt Tallac", "Mt Hood", "Mt Shasta"]
+})
+
+console.log(`
+goal: ${state.goal}
+resorts: ${JSON.stringify(state.allSkiDays)}
+fetching: ${state.resortNames.fetching}
+suggestions: ${state.resortNames.suggestions}
+
+`)
---
+++ src/store/reducers.js
@@ -0,0 +1,72 @@
+import Constants from '../constants';
+import {combineReducers} from 'redux';
+export const goal = (state = 10, action) =>
+    action.type === Constants.SET_GOAL?
+    parseInt(action.payload):
+    state;
+    
+
+export const skiDay = (state = null, action) =>
+    action.type === Constants.ADD_DAY?
+    action.payload:
+    state;
+    
+export const errors = (state=[], action) =>{
+    switch(action.type){
+        case Constants.ADD_ERROR:
+            return [
+                ...state,
+                action.payload
+            ];
+        case Constants.CLEAR_ERROR:
+            return state.filter((message, i) => i!== action.payload);
+        default:
+            return state;
+    }
+}
+
+export const allSkiDays = (state=[], action) =>{
+    
+    switch(action.type){
+        case Constants.ADD_DAY:
+            let add = [...state, skiDay(null, action)];
+            const hasDay = state.some(skiDay => skiDay.date === action.payload.date);
+            return (hasDay)? state: add;
+        case Constants.REMOVE_DAY:
+            return state.filter(skiDay => skiDay.date !== action.payload);
+        default:
+            return state;
+    }
+}
+export const fetching = (state=false, action) =>{
+    switch(action.type){
+      case Constants.CANCEL_FETCHING :
+        return false;
+      case Constants.FETCH_RESORT_NAMES :
+        return true;
+      default:
+        return state;
+    }
+  }
+
+export const suggestions= (state=[], action) =>{
+    switch(action.type) {
+        case Constants.CHANGE_SUGGESTIONS:
+        fetching(false, {type: Constants.CANCEL_FETCHING});
+        return  action.payload;
+        case Constants.CLEAR_SUGGESTIONS:
+        return [];
+        default:
+        return state;
+    }
+}
+
+export default combineReducers({
+    allSkiDays,
+    goal,
+    errors,
+    resortNames: combineReducers({
+        fetching,
+        suggestions
+    })
+})
